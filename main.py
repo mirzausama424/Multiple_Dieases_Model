09197d6fb3c7440be38758b686ce622fbf18af06
usama <Mirzausama6300@gmail.com>
2025-01-16 18:30:49 +0500

    Add untracked files and restore main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+from Auth.auth import auth_router
+from predection.predection import predection_route
+from db.database import createtable,create_users_table
+
+# Create FastAPI app instance
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+# Include routers
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(predection_route, prefix="/prediction", tags=["Prediction"])
+
+# Create the necessary tables in the database
+createtable()
+create_users_table()
+
+# Define home route
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Multiple Disease Prediction API"}
